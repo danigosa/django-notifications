8d46dcb52458f8ea152775ac06a508bf40b44b19
Danigosa <dani.gonzalez@infantium.com>
2013-12-15 21:29:32 +0100

    Update models.py
    Fix jsonfield compat

--- notifications/models.py
+++ notifications/models.py
@@ -154,7 +154,7 @@ class Notification(models.Model):
 EXTRA_DATA = False
 if getattr(settings, 'NOTIFY_USE_JSONFIELD', False):
     try:
-        from jsonfield import JSONField
+        from json_field.fields import JSONField
     except ImportError:
         raise ImproperlyConfigured("You must have a suitable JSONField installed")
     
